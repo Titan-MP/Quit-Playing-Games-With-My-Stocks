56cd90aff6d1a106764cc6e5961b90a655c7d8a5
ricguer <rj.guerra@outlook.com>
2023-09-27 19:09:42 -0400

    Refine Welcome Screen Animation Fixes #23
    - Create staggered animation sequence for "Welcome" page elements.
    - Edit "Welcome" screen animations to have glowing drop shadow.

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,5 @@
+import ChartsImage from "./analyzePositions.svg";
+import Analysis from "./buildManagePortfolio.svg";
+import Notes from "./keepStockWatchlist.svg";
+
+export { ChartsImage, Analysis, Notes };
--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -10,7 +10,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import { ColorModeContext } from "../../theme";
 import { LoginForm } from "../form";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 
                                                                 /* ==================== COMPONENTS ==================== */
@@ -99,8 +99,8 @@ const Topbar = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1, delay: 2 }}
-			sx={{ padding: "1rem", height: "10vh" }}
+            transition={{ duration: 2 }}
+			sx={{ padding: "1rem" }}
 		>
 			<Box sx={{ flexGrow: 1 }}>
 				<Box
