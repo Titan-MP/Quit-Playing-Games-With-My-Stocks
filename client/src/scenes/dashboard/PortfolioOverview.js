f73cfcf7d9afa783f4190ff993ae14593c0e4fda
ricguer <rj.guerra@outlook.com>
2023-09-29 10:58:56 -0400

    Further Define Layout of Portfolio Overview Page
    - Place a Paper element as the root element and create a grid as the main page layout.
    - Display the net liquidating value of the portfolio, the P/L YTD, and the buying power.

--- client/src/scenes/dashboard/PortfolioOverview.js
+++ client/src/scenes/dashboard/PortfolioOverview.js
@@ -21,57 +21,64 @@ const PortfolioOverview = () => {
 	};
 
     return (
-        <Paper>
-            <Grid
-                height={"100vh"}
-                container
-                spacing={2}
-                component={motion.div}
-                variants={coverContainer}
-                initial="hidden"
-                animate="show"
-            >
-                <Grid
-                    item
-                    xs={12}
-                    component={motion.div}
-                    variants={coverItem}
-                >
-                    <Typography variant="h3">Portfolio Overview</Typography>
+		<Paper>
+			<Grid
+				height={"100vh"}
+				container
+				spacing={2}
+				component={motion.div}
+				variants={coverContainer}
+				initial="hidden"
+				animate="show"
+			>
+				<Grid
+					item
+					xs={12}
+					component={motion.div}
+					variants={coverItem}
+				>
+					<Typography variant="h3">Portfolio Overview</Typography>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+					component={motion.div}
+					variants={coverItem}
+				>
+					<Typography variant="h6">Net Liq.</Typography>
+					<Typography variant="h4">$100,000.00</Typography>
+					<Typography variant="h6">P/L YTD</Typography>
+					<Typography variant="h4">$10,000.00</Typography>
+					<Typography variant="h6">Buying Power</Typography>
+					<Typography variant="h4">$50,000.00</Typography>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+					component={motion.div}
+					variants={coverItem}
+				></Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+					component={motion.div}
+					variants={coverItem}
+				></Grid>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+					component={motion.div}
+					variants={coverItem}
+				>
+                    <Typography variant="h6">Positions</Typography>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={4}
-                    component={motion.div}
-                    variants={coverItem}
-                    height={"75vh"}
-                >
-                    <Typography variant="h5">Net Liq.</Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={4}
-                    component={motion.div}
-                    variants={coverItem}
-                    height={"75vh"}
-                >
-                    <Typography variant="h5">P/L YTD</Typography>
-                </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={4}
-                    component={motion.div}
-                    variants={coverItem}
-                    height={"75vh"}
-                >
-                    <Typography variant="h5">Buying Power</Typography>
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+			</Grid>
+		</Paper>
+	);
 };
 
 export default PortfolioOverview;
