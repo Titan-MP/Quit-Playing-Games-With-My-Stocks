8e4c42720d221c581160c28b645f0dba813878d2
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-09-29 20:22:59 -0400

    removed server test

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,32 +3,3 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/stocks_db');
 
 module.exports = mongoose.connection;
-
-const { MongoClient } = require("mongodb");
-
-const username = encodeURIComponent("<username>");
-const password = encodeURIComponent("<password>");
-const cluster = "<clusterName>";
-const authSource = "<authSource>";
-const authMechanism = "<authMechanism>";
-
-let uri =
-  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
-
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    await client.connect();
-
-    const database = client.db("<dbName>");
-    const ratings = database.collection("<collName>");
-
-    const cursor = ratings.find();
-
-    await cursor.forEach(doc => console.dir(doc));
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
