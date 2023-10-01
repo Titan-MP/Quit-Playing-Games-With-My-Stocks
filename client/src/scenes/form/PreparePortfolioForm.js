6eee2eaa8007f72f32c05b0a60c378f235482a8c
ricguer <rj.guerra@outlook.com>
2023-10-01 14:42:01 -0400

    Wrap "PreparePortfolioForm" in a Box

--- client/src/scenes/form/PreparePortfolioForm.js
+++ client/src/scenes/form/PreparePortfolioForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, TextField } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import InputAdornment from '@mui/material/InputAdornment';
@@ -23,60 +23,73 @@ const userSchema = yup.object().shape({
                                                                 /* ------------- FORM: PREPARE PORTFOLIO -------------- */
 const PreparePortfolioForm = () => {
     return (
-        <React.Fragment>
-            <Typography
-                variant="h3"
-                align="center"
-                paddingBottom="30px"
-            >
-                Prepare Your Portfolio
-            </Typography>
-            <Typography
-                variant="h5"
-                align="center"
-                paddingBottom="30px"
-            >
-                How much money would you like to start with?
-            </Typography>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={userSchema}
-                onSubmit={(values) => {
-                    console.log(values);
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <TextField
-                            margin="dense"
-                            id="initialFunding"
-                            label="Initial Funding"
-                            type="number"
-                            fullWidth
-                            value={values.initialFunding}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            error={touched.initialFunding && Boolean(errors.initialFunding)}
-                            helperText={touched.initialFunding && errors.initialFunding}
-                            inputProps={{ min: "0" }} // Prevents the value from going below 0
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">$</InputAdornment>
-                ) // Adds a dollar sign as a start adornment
-              }}
-                        />
-                    </form>
-                )}
-            </Formik>
-        </React.Fragment>
-    );
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center"
+			}}
+		>
+			<Typography
+				variant="h3"
+				align="center"
+				paddingBottom="30px"
+			>
+				Prepare Your Portfolio
+			</Typography>
+			<Typography
+				variant="h5"
+				align="center"
+				paddingBottom="30px"
+			>
+				How much money would you like to start with?
+			</Typography>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={userSchema}
+				onSubmit={(values) => {
+					console.log(values);
+				}}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit
+				}) => (
+					<form onSubmit={handleSubmit}>
+						<TextField
+							margin="dense"
+							id="initialFunding"
+							label="Initial Funding"
+							type="number"
+							fullWidth
+							value={values.initialFunding}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							error={
+								touched.initialFunding &&
+								Boolean(errors.initialFunding)
+							}
+							helperText={
+								touched.initialFunding && errors.initialFunding
+							}
+							inputProps={{ min: "0" }} // Prevents the value from going below 0
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										$
+									</InputAdornment>
+								) // Adds a dollar sign as a start adornment
+							}}
+						/>
+					</form>
+				)}
+			</Formik>
+		</Box>
+	);
 }
 
 export default PreparePortfolioForm;
